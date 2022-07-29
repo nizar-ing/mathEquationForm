84fcae7f8967aac23311dbc026b4bdd850ee5530
Nizar ILAHI <nizar.ilahi@gmail.com>
2022-07-29 18:54:25 +0100

    second commit ... applying some styling by adding classes dynamically based on directive concept and rxjs as well :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EquationComponent } from './equation/equation.component';
+import { AnswerHighlightDirective } from './answer-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EquationComponent
+    EquationComponent,
+    AnswerHighlightDirective
   ],
   imports: [
     BrowserModule,
